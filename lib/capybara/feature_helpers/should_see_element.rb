a03d4bb58604b3906029925412df657af2a95259
Kirill Mikhailets <kmikhailets@brightbytes.net>
2019-05-09 14:25:04 +0300

    Added normalize_ws: true to element matcher

--- lib/capybara/feature_helpers/should_see_element.rb
+++ lib/capybara/feature_helpers/should_see_element.rb
@@ -2,11 +2,11 @@ module Capybara
   module FeatureHelpers
 
     def should_see_element(selector, options = {})
-      expect(page).to have_selector(selector, options)
+      expect(page).to have_selector(selector, options.merge(normalize_ws: true))
     end
 
     def should_not_see_element(selector, options = {})
-      expect(page).not_to have_selector(selector, options.merge(visible: true))
+      expect(page).not_to have_selector(selector, options.merge(normalize_ws: true, visible: true))
     end
 
   end
